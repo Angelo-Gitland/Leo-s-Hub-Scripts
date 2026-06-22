5a8bf0354ff7a59f9342ad8f16c9f254411e90ef
LLH Lua Land <angelo.olavere64@gmail.com>
2026-06-22 08:50:38 +0800

    Whitelisted tmanmonks

--- Main.lua
+++ Main.lua
@@ -2,6 +2,12 @@ local allowedUsers = {
     [3673634114] = "permanent",
     [4321575771] = "permanent",
     [2235731985] = "permanent",
+    [2242336104] = os.time({
+        year = 2026,
+        month = 7,
+        day = 22,
+        hour = 0
+    }),
     [9851659701] = os.time({
         year = 2026,
         month = 7,
