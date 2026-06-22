4a991da577c6c46c069314b3ea716ef446d6a875
LLH Lua Land <angelo.olavere64@gmail.com>
2026-06-22 11:30:42 +0800

    Whitelisted peterson_execrable

--- Main.lua
+++ Main.lua
@@ -2,6 +2,12 @@ local allowedUsers = {
     [3673634114] = "permanent",
     [4321575771] = "permanent",
     [2235731985] = "permanent",
+    [10512592607] = os.time({
+        year = 2026,
+        month = 7,
+        day = 22,
+        hour = 3
+    }),
     [2242336104] = os.time({
         year = 2026,
         month = 7,
