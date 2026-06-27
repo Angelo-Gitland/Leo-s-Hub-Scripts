1931ee78a34145a69804053352ae6d044b466976
LLH Lua Land <angelo.olavere64@gmail.com>
2026-06-27 15:59:20 +0800

    Whitelisted AspringSmellybum2

--- Main.lua
+++ Main.lua
@@ -1,6 +1,7 @@
 local allowedUsers = {
     [3673634114] = "permanent",
     [4256101131] = "permanent",
+    [AspringSmellybum2] = "permanent",
     [4321575771] = "permanent",
     [2235731985] = "permanent",
     [8081233261] = os.time({
